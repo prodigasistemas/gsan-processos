96522f49cc52292bf1f617955464bdad82b255e1
Marcelo de Brito Garcia <mbgarcia_pa@yahoo.com.br>
2015-02-25 10:27:21 -0300

    Configuracao de ConnectionFactory

--- src/main/java/br/gov/mensageiros/ProcessoMensageiro.java
+++ src/main/java/br/gov/mensageiros/ProcessoMensageiro.java
@@ -3,23 +3,32 @@ package br.gov.mensageiros;
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.jms.JMSConnectionFactory;
 import javax.jms.JMSContext;
 import javax.jms.ObjectMessage;
 import javax.jms.Queue;
+
+import org.jboss.logging.Logger;
 
 import br.gov.model.batch.ProcessoIniciado;
 
 @Stateless
 public class ProcessoMensageiro {
+    private static Logger logger = Logger.getLogger(ProcessoMensageiro.class);
 
 	@Inject
+	@JMSConnectionFactory("java:jboss/DefaultJMSConnectionFactory")
 	JMSContext contexto;
 
 	@Resource(mappedName="java:global/jms/processosFila")
 	Queue processosFila;
 
 	public void enviarParaFila(ProcessoIniciado processoIniciado) {
-		ObjectMessage mensagem = contexto.createObjectMessage(processoIniciado);
-		contexto.createProducer().send(processosFila, mensagem);
+	    try {
+	        ObjectMessage mensagem = contexto.createObjectMessage(processoIniciado);
+	        contexto.createProducer().send(processosFila, mensagem);
+        } catch (Exception e) {
+            logger.error("Erro ao enviar processo para fila", e);
+        }
 	}
 }
