38f7fa28e49ea2d2fd6f736f073c88ea1d09535f
Marcelo de Brito Garcia <mbgarcia_pa@yahoo.com.br>
2015-06-24 09:55:35 -0300

    Escrita de log em uma pasta paratrizada

--- src/main/java/br/gov/agendadores/GerenciadorLog.java
+++ src/main/java/br/gov/agendadores/GerenciadorLog.java
@@ -6,16 +6,29 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
+import org.jboss.logging.Logger;
+
+import br.gov.mensageiros.ProcessoMensageiro;
 import br.gov.model.batch.ProcessoIniciado;
+import br.gov.model.seguranca.SegurancaParametro;
+import br.gov.servicos.seguranca.SegurancaParametroRepositorio;
 
 @Stateless
 public class GerenciadorLog {
+    private static Logger logger = Logger.getLogger(ProcessoMensageiro.class);
 
-	private static final String ROOT_PATH = "/var/tmp/";
+	private String ROOT_PATH = "/var/tmp/";
+	
+	@EJB
+	private SegurancaParametroRepositorio repositorioParametros;
 	
 	public void reiniciaLog(ProcessoIniciado processoIniciado) {
+	    ROOT_PATH = repositorioParametros.recuperaPeloNome(SegurancaParametro.NOME_PARAMETRO_SEGURANCA.CAMINHO_LOG_BATCH);
+	    
 	    File file = new File(ROOT_PATH + processoIniciado.getId() + "_iniciado.log");
 
 	    File logFile = new File(ROOT_PATH + processoIniciado.getId() + ".log");
@@ -32,7 +45,10 @@ public class GerenciadorLog {
 	}
 	
 	public void gravandoLog(String idProcesso, String mensagem, boolean append) {
+	    ROOT_PATH = repositorioParametros.recuperaPeloNome(SegurancaParametro.NOME_PARAMETRO_SEGURANCA.CAMINHO_LOG_BATCH);
+	    
 		try {
+
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
 				    new FileOutputStream(ROOT_PATH + idProcesso + "_iniciado.log", append), "UTF-8"));
 			
@@ -41,7 +57,7 @@ public class GerenciadorLog {
 			
 			bw.close();
 		} catch (IOException e) {
-			e.printStackTrace();
+		    logger.error("Erro ao gravar log", e);
 		}
     }
 }
